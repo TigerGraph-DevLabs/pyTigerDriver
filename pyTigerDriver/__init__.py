b9b1d908fad6450ae41a88f24999a16be0c6d654
Zrouga Mohamed <61499067+zrougamed@users.noreply.github.com>
2021-09-01 16:52:02 +0100

    fixing the progressbar , fixing the graph ...

--- pyTigerDriver/__init__.py
+++ pyTigerDriver/__init__.py
@@ -6,6 +6,6 @@ from .pyDriver import GSQL_Client, ExceptionRecursiveRet, ExceptionCodeRet
 class Client():
     def __init__(self, server_ip="127.0.0.1", username="tigergraph", password="tigergraph", cacert=""
                  ,version="", commit="",gsPort="14240",restpp="9000",debug=False):
-        self.Rest = REST_Client(server_ip)
+
         self.Gsql = GSQL_Client(server_ip=server_ip, username=username, password=password, cacert=cacert
                            ,version=version, commit=commit,gsPort=gsPort,restpp=restpp,debug=debug)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as rdm:
 
 setuptools.setup(
     name="pyTigerDriver",
-    version="v1.0.13",
+    version="v1.0.14",
     author="Zrouga Mohamed",
     author_email="medzrouga@gmail.com",
     description="GSQL client for TigerGraph",
